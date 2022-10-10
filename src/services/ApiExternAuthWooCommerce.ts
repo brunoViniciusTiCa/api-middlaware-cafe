d05bb36aa9e3d728eafca71187500f2a09ff72af
brunovinicius <brunoviniciustica@gmail.com>
2022-10-10 10:07:25 +0100

    Refactoring Code

--- src/services/ApiExternAuthWooCommerce.ts
+++ src/services/ApiExternAuthWooCommerce.ts
@@ -2,10 +2,12 @@ const axios = require('axios').default
 
 class AuthWooCommerce {
     public async wooCommerceFunc(consumerKey: String, consumerSecret: String) {
+        const pathUrl = await `${process.env.URL_WOOCOMMERCE}/products?`
+        const consumersKey = await `consumer_key=${consumerKey}` 
+        const consumersSecret = await `&consumer_secret=${consumerSecret}` 
 
-        const api = await axios.get(
-            `${process.env.URL_WOOCOMMERCE}/products?consumer_key=${consumerKey}&consumer_secret=${consumerSecret}`
-        )
+        const urlFull = await (pathUrl+consumersKey+consumersSecret);
+        const api = await axios.get(urlFull);
         return api.data;
     }
 }
